9ab9be453c2003e451239d003ef5d4227e34958a
dhershman <dhershman@dealertire.com>
2017-09-11 14:47:32 -0400

    Moar unit tests

--- src/tests/equals.js
+++ src/tests/equals.js
@@ -69,3 +69,121 @@ test('Test handles regex', t => {
 	t.notOk(isEqual(/a/gi, /a/i), 'Handles not equal flags');
 	t.end();
 });
+
+test('Test handles recursive data', t => {
+	const c = {};
+	const d = {};
+	const e = [];
+	const f = [];
+
+	c.v = c;
+	d.v = d;
+	e.push(e);
+	f.push(f);
+
+	const nestA = {
+		a: [1, 2, {c: 1}],
+		b: 1
+	};
+	const nestB = {
+		a: [1, 2, {c: 1}],
+		b: 1
+	};
+	const nestC = {
+		a: [1, 2, {c: 2}],
+		b: 1
+	};
+
+	t.ok(isEqual(c, d), 'objects are equal');
+	t.ok(isEqual(e, f), 'arrays are equal');
+	t.notOk(isEqual(c, f), 'objects are not equal to arrays');
+	t.notOk(isEqual(d, e), 'objects are not equal to arrays');
+	t.ok(isEqual(nestA, nestB), 'nested A matches other nested data B');
+	t.notOk(isEqual(nestA, nestC), 'nested A does not match other nested data C');
+	t.end();
+});
+
+test('Test handles dates', t => {
+	t.ok(isEqual(new Date(0), new Date(0)), 'New dates of 0 match');
+	t.ok(isEqual(new Date(1), new Date(1)), 'New dates of 1 match');
+	t.notOk(isEqual(new Date(1), new Date(0)), 'New dates of 1 and 0 do not match');
+	t.end();
+});
+
+test('Test requires both objects to have the same enumerable properties with the same value', t => {
+
+	const a1 = [];
+	const a2 = [];
+
+	a2.x = 0;
+
+	const b1 = new Boolean(false);
+	const b2 = new Boolean(false);
+
+	b2.x = 0;
+
+	const d1 = new Date(0);
+	const d2 = new Date(0);
+
+	d2.x = 0;
+
+	const n1 = new Number(0);
+	const n2 = new Number(0);
+
+	n2.x = 0;
+
+	const r1 = /(?:)/;
+	const r2 = /(?:)/;
+
+	r2.x = 0;
+
+	const s1 = new String('');
+	const s2 = new String('');
+
+	s2.x = 0;
+
+	t.notOk(isEqual(a1, a2), 'Array enumerables not equal');
+	t.notOk(isEqual(b1, b2), 'Boolean enumerables not equal');
+	t.notOk(isEqual(d1, d2), 'Date enumerables not equal');
+	t.notOk(isEqual(n1, n2), 'Number enumerables not equal');
+	t.notOk(isEqual(r1, r2), 'Regex enumerables not equal');
+	t.notOk(isEqual(s1, s2), 'String enumerables not equal');
+	t.end();
+});
+
+test('Test handles typed arrays', t => {
+	if (typeof ArrayBuffer !== 'undefined' && typeof Int8Array !== 'undefined') {
+		const typArr1 = new ArrayBuffer(10);
+		const typArr2 = new ArrayBuffer(10);
+		const typArr3 = new ArrayBuffer(10);
+
+		typArr1[0] = 1;
+		typArr2[0] = 1;
+		typArr3[0] = 0;
+		const intTypArr = new Int8Array(typArr1);
+
+		t.ok(isEqual(typArr1, typArr2), 'type array buffer is equal to another array buffer');
+		t.notOk(isEqual(typArr1, typArr3), 'Type array buffer 3 has different index 0');
+		t.notOk(isEqual(typArr1, intTypArr), 'Type array not equal to int type array');
+	}
+
+	t.end();
+});
+
+test('Test handles promise objects with identity', t => {
+	const p = Promise.resolve(10);
+	const q = Promise.resolve(10);
+
+	t.ok(isEqual(p, p), 'First promise identity matches second promise');
+	t.notOk(isEqual(p, q), 'First promise does not match second promise identity');
+	t.end();
+});
+
+test('Test compares map objects by value', t => {
+	t.ok(isEqual(new Map([]), new Map([])), 'Empty map equals empty map');
+	t.notOk(isEqual(new Map([]), new Map([[1, 'a']])), 'Empty map does not equal item map');
+	t.ok(isEqual(new Map([[1, 'a']]), new Map([[1, 'a']])), 'Item map with same data equals other item map');
+	t.ok(isEqual(new Map([[1, 'a'], [2, 'b']]), new Map([[1, 'a'], [2, 'b']])), 'Multi position data still matches');
+	t.ok(isEqual(new Map([[1, 'a'], [2, new Map([[3, 'c']])]]), new Map([[1, 'a'], [2, new Map([[3, 'c']])]])), 'mapception');
+	t.end();
+});
