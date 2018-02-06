374acb4381642be97fa25136aaa5e90130ffe478
dhershman <dhershman@dealertire.com>
2018-02-06 13:20:26 -0500

    contains returns a boolean now, added documentation to contains, updated docs.js

--- src/contains/index.js
+++ src/contains/index.js
@@ -1,19 +1,39 @@
 import curry from '../curry';
 import isEqual from '../isEqual';
 
+/**
+ * @since v0.5.0
+ * @sig a → [a] → Boolean
+ * @description
+ * Checks to see if the provided list contains at at least 1 of the provided value within it
+ * @param {Any} a The value we want to search the list for
+ * @param {Array} list The list we want to search through
+ * @return {Boolean} A Boolean based on if the value is found or not
+ *
+ * @example
+ * contains(3, [1, 2, 3]); // => true
+ * contains({ value: 1 }, [{ value: 1 }, { value: 2 }]); // => true
+ * contains([42], [[42]]); // => true
+ *
+ * // It is also curried
+ *
+ * const checker = contains(3);
+ *
+ * checker([1, 2, 3]); // => true
+ */
 const contains = curry((a, list) => {
   let idx = 0;
   const len = list.length;
 
   while (idx < len) {
     if (isEqual(list[idx], a)) {
-      return idx;
+      return true;
     }
 
     idx += 1;
   }
 
-  return -1;
+  return false;
 });
 
 export default contains;
--- src/contains/tests.js
+++ src/contains/tests.js
@@ -4,19 +4,19 @@ import test from 'ava';
 test('Basic simple functionality', t => {
   const list = [1, 2, 3, 4];
 
-  t.is(contains(2, list), 1);
-  t.is(contains(4, list), 3);
+  t.truthy(contains(2, list));
+  t.truthy(contains(4, list));
 });
 
 test('Basic complex functionality', t => {
   const list = [{ value: 1 }, { value: 2 }];
 
-  t.is(contains({ value: 1 }, list), 0);
+  t.truthy(contains({ value: 1 }, list));
 });
 
 test('Basic curry functionality', t => {
   const list = [1, 2, 3];
   const checker = contains(3);
 
-  t.is(checker(list), 2);
+  t.truthy(checker(list));
 });
