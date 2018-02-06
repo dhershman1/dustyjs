5b8e934127209a73f3efc779294348a2b0e5c025
dhershman <dhershman@dealertire.com>
2018-02-06 13:03:57 -0500

    Added Docs for filter and reject

--- src/filter/index.js
+++ src/filter/index.js
@@ -1,6 +1,27 @@
 import curry from '../curry';
 import reduce from '../_internals/reduce';
 
+/**
+ * @since v0.5.0
+ * @sig Filterable f => (a → Boolean) → f a → f a
+ * @description Filter through a filterable data piece using the provided function
+ * @param {Function} fn The predicate function to run on our values
+ * @param {Array|Object} list The filterable list to go through
+ * @return {Array|Object} Returns a new Array or Object based on the type of list provided
+ *
+ * @example
+ * const isEven = n => n % 2 === 0;
+ *
+ * filter(isEven, [1, 2, 3, 4]); // => [2, 4]
+ * filter(isEven, { a: 1, b: 2, c: 3, d: 4 }); // => { b: 2, d: 4 }
+ *
+ * // Is also curried
+ *
+ * const filterer = filter(isEven);
+ *
+ * filterer([1, 2, 3, 4]); // => [2, 4]
+ * filterer({ a: 1, b: 2, c: 3, d: 4 }); // => { b: 2, d: 4 }
+ */
 const filter = curry((fn, list) => reduce(fn, list));
 
 export default filter;
--- src/reject/index.js
+++ src/reject/index.js
@@ -2,6 +2,28 @@ import complement from '../_internals/complement';
 import curry from '../curry';
 import filter from '../filter';
 
+/**
+ * @since v0.5.0
+ * @sig Filterable f => (a → Boolean) → f a → f a
+ * @description
+ * Filter through a filterable data piece using the provided function returns only those that fail the function test
+ * @param {Function} fn The predicate function to run on our values
+ * @param {Array|Object} list The filterable list to go through
+ * @return {Array|Object} Returns a new Array or Object based on the type of list provided
+ *
+ * @example
+ * const isEven = n => n % 2 === 0;
+ *
+ * reject(isEven, [1, 2, 3, 4]); // => [1, 3]
+ * reject(isEven, { a: 1, b: 2, c: 3, d: 4 }); // => { a: 1, c: 3 }
+ *
+ * // Is also curried
+ *
+ * const rejecter = reject(isEven);
+ *
+ * rejecter([1, 2, 3, 4]); // => [1, 3]
+ * rejecter({ a: 1, b: 2, c: 3, d: 4 }); // => { a: 1, c: 3 }
+ */
 const reject = curry((fn, list) => filter(complement(fn), list));
 
 export default reject;
