1b6de825b5352d3f170a18e21145b19af76b621f
dhershman1 <dustinh17@gmail.com>
2018-02-27 17:41:11 -0500

    Added or and the and functions

---
+++ src/or/tests.js
@@ -0,0 +1,28 @@
+import or from './index';
+import test from 'ava';
+
+test('Returns true when both params are true', t => {
+  t.true(or(true, true));
+});
+
+test('Returns true when one param is false', t => {
+  t.true(or(true, false));
+});
+
+test('Returns false when both params are false', t => {
+  t.false(or(false, false));
+});
+
+test('Is curried true', t => {
+  const a = or(true);
+
+  t.true(a(true));
+  t.true(a(false));
+});
+
+test('Is curried false', t => {
+  const a = or(false);
+
+  t.true(a(true));
+  t.false(a(false));
+});
