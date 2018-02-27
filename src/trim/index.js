402ceededc17a7fdfd7f69538e549b63e601f030
dhershman <dhershman@dealertire.com>
2018-02-27 13:11:23 -0500

    Add trim function

---
+++ src/trim/index.js
@@ -0,0 +1,15 @@
+
+/**
+ * @name trim
+ * @since v2.0.0
+ * @category String
+ * @sig String a -> String a
+ * @description
+ * Accepts a string value and trims it's white space
+ * @param {String} str The string to trim
+ * @return {String} The trimmed string
+ * @example
+ * trim('my new cow   '); // => 'my new cow'
+ * trim('   new things   '); // => 'new things'
+ */
+export default str => str.trim();
