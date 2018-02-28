5a9b7348dd6308f8b221e0b55e13c2c73905d9a2
dhsershman1 <dustinh17@gmail.com>
2018-02-28 10:45:48 -0500

    Create Reverse Function

---
+++ src/reverse/index.js
@@ -0,0 +1,15 @@
+
+/**
+ * @name reverse
+ * @since v2.0.0
+ * @category Array
+ * @sig Array a -> Array a
+ * @description Accepts an array and returns a brand new reversed array
+ * @param {Array} arr The array to reverse
+ * @return {Array} A new reversed array
+ *
+ * @example
+ * reverse([1, 2, 3]); // => [3, 2, 1]
+ * reverse([]); // => []
+ */
+export default arr => arr.slice().reverse();
