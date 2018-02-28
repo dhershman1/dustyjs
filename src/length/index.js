bf51bbb891a0c438cce7865b6a0f195560fa6497
dhsershman1 <dustinh17@gmail.com>
2018-02-28 10:35:09 -0500

    Added length function

---
+++ src/length/index.js
@@ -0,0 +1,15 @@
+
+/**
+ * @name length
+ * @since v2.0.0
+ * @category Array
+ * @sig Array a -> Number
+ * @description Obtains the length of the passed array
+ * @param {Array} a The array to find the length of
+ * @return {Number} The length of the array
+ *
+ * @example
+ * length([1, 2, 3, 4]); // => 4
+ * length([]); // => 0
+ */
+export default a => a.length;
