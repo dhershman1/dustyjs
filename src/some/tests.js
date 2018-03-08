5e359c99456e43e7ca92ceb610017c56ca7dd7bf
dhsershman1 <dustinh17@gmail.com>
2018-03-08 11:38:46 -0500

    Rename any to some

--- src/some/tests.js
+++ src/some/tests.js
@@ -1,16 +1,16 @@
-import any from './';
+import some from './';
 import test from 'ava';
 
 test('Basic functionality', t => {
   const runner = x => typeof x === 'string';
 
-  t.truthy(any(runner, ['foo', 'bar']));
-  t.truthy(any(runner, ['foo', 'bar', 1]));
-  t.falsy(any(runner, [1, 2, 3]));
+  t.truthy(some(runner, ['foo', 'bar']));
+  t.truthy(some(runner, ['foo', 'bar', 1]));
+  t.falsy(some(runner, [1, 2, 3]));
 });
 
 test('Basic curried functionality', t => {
-  const a = any(x => typeof x === 'string');
+  const a = some(x => typeof x === 'string');
 
   t.truthy(a(['foo', 'bar']));
   t.truthy(a(['foo', 'bar', 1]));
@@ -20,6 +20,6 @@ test('Basic curried functionality', t => {
 test('Run against objects', t => {
   const runner = ({ val }) => val === 'test';
 
-  t.truthy(any(runner, [{ val: 'thing' }, { val: 'test' }]));
-  t.falsy(any(runner, [{ val: 'thing' }, { val: 100 }]));
+  t.truthy(some(runner, [{ val: 'thing' }, { val: 'test' }]));
+  t.falsy(some(runner, [{ val: 'thing' }, { val: 100 }]));
 });
