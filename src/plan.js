b92842efe19bfcbcd50aaa2eddf7d61da72734a1
dhershman1 <dustinh17@gmail.com>
2018-05-12 12:45:34 -0400

    Fix plan docs

--- src/plan.js
+++ src/plan.js
@@ -7,6 +7,22 @@ import curry from './curry'
  * @category Object
  * @sig Object -> Object -> Object
  * @description Uses a schema to apply a map like functionality on a given object
+ * @param {Object} schema The object of functions we want to apply
+ * @param {Object} obj The object to apply our functions too
+ * @return {Object} A new object with the updated data from our applied functions
+ * @example
+ * const testFns = {
+    a: x => x * 2,
+    b: x => x + 10
+  }
+ *
+ * plan(testFns, { a: 5, b: 10 }) // => { a: 10, b: 20 }
+ *
+ * // It's also curried
+ *
+ * const p = plan(testFns)
+ *
+ * p({ a: 5, b: 10 }) // => { a: 10, b: 20 }
  */
 const plan = (schema, obj) =>
   assign({}, obj, Object.keys(obj).reduce((acc, k) => {
